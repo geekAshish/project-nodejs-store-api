794161460c32f5b58b73f0bf354832f2500ccb99
Ashish Kushwaha <ashish@wishfy.club>
2024-12-31 23:08:01 +0530

    store api init

--- src/controller/products.ts
+++ src/controller/products.ts
@@ -1,9 +1,10 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { asyncWrapper } from "../middleware/async";
 
 export const getAllProductsStatic = asyncWrapper(
-  async (req: Request, res: Response) => {
+  async (req: Request, res: Response, next: NextFunction) => {
     res.status(200).json({ name: "name" });
+    // next({ error: "this is error" });
   }
 );
 export const getAllProducts = asyncWrapper(
